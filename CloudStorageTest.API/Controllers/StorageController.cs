e57a14b0561ac101343bc63116e13f26208e0f7f
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2024-03-30 17:41:53 -0300

    feat(profile): add simple upload

---
+++ CloudStorageTest.API/Controllers/StorageController.cs
@@ -0,0 +1,19 @@
+﻿using CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorageTest.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : Controller
+{
+    [HttpPost]
+    public IActionResult UploadImage(IFormFile file)
+    {
+        var useCase = new UploadProfilePhotoUseCase();
+
+        useCase.Execute(file);
+        
+        return Created();
+    }
+}
---
+++ CloudStorageTest.Application/UseCases/Users/UploadProfilePhoto/UploadProfilePhotoUseCase.cs
@@ -0,0 +1,20 @@
+﻿using FileTypeChecker.Extensions;
+using FileTypeChecker.Types;
+using Microsoft.AspNetCore.Http;
+
+namespace CloudStorageTest.Application.UseCases.Users.UploadProfilePhoto;
+
+public class UploadProfilePhotoUseCase
+{
+    public void Execute(IFormFile file)
+    {
+        var fileStream = file.OpenReadStream();
+
+        var isImage = fileStream.Is<JointPhotographicExpertsGroup>();
+
+        if (isImage == false)
+        {
+            throw new Exception("This file is not an image.");
+        }
+    }
+}
